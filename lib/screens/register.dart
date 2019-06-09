1b927f551a4d0b4ff08cfc7e38250f6609c68a99
Thanayos <ithanayos@gmail.com>
2019-06-09 11:37:16 +0700

    Get Value Fron Field Success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,10 +6,18 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final formKey = GlobalKey<FormState>();
+  String nameString, emailString, passwordString;
+
   Widget uploadButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Name = $nameString, Email = $emailString, Password = $passwordString');
+        }
+      },
     );
   }
 
@@ -19,7 +27,16 @@ class _RegisterState extends State<Register> {
       child: Container(
         width: 250.0,
         child: TextFormField(
-          decoration: InputDecoration(labelText: 'Name :', hintText: 'Request'),
+          decoration: InputDecoration(
+            labelText: 'Name :',
+            hintText: 'Request',
+          ),
+          validator: (String value) {
+            if (value.length == 0) {
+              return 'Please Fill Name In Blank';
+            }
+          },
+          onSaved: (String value) => nameString = value,
         ),
       ),
     );
@@ -31,8 +48,15 @@ class _RegisterState extends State<Register> {
       child: Container(
         width: 250.0,
         child: TextFormField(
-          decoration:
-              InputDecoration(labelText: 'Email :', hintText: 'you@abc.com'),
+          decoration: InputDecoration(
+            labelText: 'Email :',
+            hintText: 'you@abc.com',
+          ),
+          validator: (String value) {
+            if (!((value.contains('@')) && (value.contains('.')))) {
+              return 'Please Type Email Format';
+            }
+          },onSaved: (String value){emailString = value;},
         ),
       ),
     );
@@ -45,7 +69,14 @@ class _RegisterState extends State<Register> {
         width: 250.0,
         child: TextFormField(
           decoration: InputDecoration(
-              labelText: 'Password :', hintText: 'More 6 Charactor'),
+            labelText: 'Password :',
+            hintText: 'More 6 Charactor',
+          ),
+          validator: (String value) {
+            if (value.length <= 5) {
+              return 'More 6 Charactor';
+            }
+          },onSaved: (String value) => passwordString = value,
         ),
       ),
     );
@@ -59,15 +90,18 @@ class _RegisterState extends State<Register> {
         title: Text('Register'),
         actions: <Widget>[uploadButton()],
       ),
-      body: Container(
-        padding: EdgeInsets.only(top: 80.0),
-        alignment: Alignment(0, -1),
-        child: Column(
-          children: <Widget>[
-            nameTextFormField(),
-            emailTextFormField(),
-            passwordTextFormField()
-          ],
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.only(top: 80.0),
+          alignment: Alignment(0, -1),
+          child: Column(
+            children: <Widget>[
+              nameTextFormField(),
+              emailTextFormField(),
+              passwordTextFormField()
+            ],
+          ),
         ),
       ),
     );
